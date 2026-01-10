bd1d2ffbe8255d7e3efd94cef933810096ce9bcf
Vicent  Mwanda <vicent@aims.ac.za>
2026-01-10 14:17:33 +0200

    new 8

--- my_recommender_package/model.py
+++ my_recommender_package/model.py
@@ -444,9 +444,10 @@ Duration: {time_stamp:.3f} seconds
           user_vector = np.zeros(K)
 
           n_ratings = len(user_ratings)
-
-          effective_tau = tau * min(n_ratings / epochs, 1.0)
-          effective_gamma = gamma * min(n_ratings / epochs, 1.0)
+          
+          #lower overfitting for users with very few ratings
+          effective_tau = tau 
+          effective_gamma = gamma 
 
           for epoch in range(epochs):
 
