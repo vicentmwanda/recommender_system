48e320938354812942bc20b57d84fbbcabfc97ac
Vicent  Mwanda <vicent@aims.ac.za>
2026-01-10 14:33:13 +0200

    new 10

--- my_recommender_package/model.py
+++ my_recommender_package/model.py
@@ -445,7 +445,7 @@ Duration: {time_stamp:.3f} seconds
 
           n_ratings = len(user_ratings)
           
-          #lower overfitting for users with very few ratings
+          #to avoid underfitting for users with very few ratings
           effective_tau = tau * min(n_ratings / epochs, 1.0)
           effective_gamma = gamma * min(n_ratings / epochs, 1.0)
 
